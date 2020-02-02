60397b6f6ef5dac4e9d0e05ecaaa503c8df4ce07
Justin <twoplatinumchainz@gmail.com>
2020-02-02 15:46:20 -0800

    test WIP

--- test/test.js
+++ test/test.js
@@ -5,16 +5,27 @@ const html = require('../htmlTemplate.js');
 
 describe('Index', () => {
     describe('promptUser', () => {
-        it('should ask the user for their username and favoite color, then save them', async () => {
-            const answers = await index.promptUser();
+        it('should take the users input and save it to be used', async () => {
+            const actual = await promptUser();
 
-            expect(answers.github).toEqual('twopcz');
+            expect(answers.github).toEqual('test');
         });
     });
 });
 
-// describe('generateHTML function', () => {
-//     it('populates the template with the GitHub user\'s information', () => {
 
-//     })
-// })
+// describe("Describe Inquirer function", () => {
+//     it("should validate user inputs from the terminal and store information", () => {
+//       const input = 'GitHub1'
+//       const output = 'GitHub1'
+//       expect(input).toEqual(output);
+//     });
+//     it("should validate user inputs from the terminal and store information", () => {
+//         const input = '';
+  
+//         const err = new Error (
+//             `Invalid input something something something`
+//         )
+//         expect(input).toThrowError(err);
+//       });
+// });
