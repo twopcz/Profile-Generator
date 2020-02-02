b0e6877f602636f9b82dffd04077799446734453
Justin <twoplatinumchainz@gmail.com>
2020-02-02 13:21:12 -0800

    initial unit testing

--- test/test.js
+++ test/test.js
@@ -1,3 +1,20 @@
 'use strict';
 
 const index = require('../index.js');
+const html = require('../htmlTemplate.js');
+
+describe('Index', () => {
+    describe('promptUser', () => {
+        it('should ask the user for their username and favoite color, then save them', async () => {
+            const answers = await index.promptUser();
+
+            expect(answers.github).toEqual('twopcz');
+        });
+    });
+});
+
+// describe('generateHTML function', () => {
+//     it('populates the template with the GitHub user\'s information', () => {
+
+//     })
+// })
